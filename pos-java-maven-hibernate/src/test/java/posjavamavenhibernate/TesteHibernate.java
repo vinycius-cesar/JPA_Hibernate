8392b9e15baec9283f71a11c9e65e341ace86117
vinycius-cesar <68862312+vinycius-cesar@users.noreply.github.com>
2021-03-09 16:53:13 -0300

    comit

---
+++ pos-java-maven-hibernate/src/test/java/posjavamavenhibernate/TesteHibernate.java
@@ -0,0 +1,10 @@
+package posjavamavenhibernate;
+
+import org.junit.Test;
+
+public class TesteHibernate {
+	@Test
+     public void testeHibernateUtil() {
+    	HibernateUtil.getEntityManager();
+     }
+}
