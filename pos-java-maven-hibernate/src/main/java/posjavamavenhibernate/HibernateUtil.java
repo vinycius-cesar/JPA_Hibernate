76e11055c38d78610ff4532e12d6cecdb92caba0
vinycius-cesar <68862312+vinycius-cesar@users.noreply.github.com>
2021-03-09 21:49:01 -0300

    atualização
    <property name="hibernate.hbm2ddl.auto" value="create"/>

--- pos-java-maven-hibernate/src/main/java/posjavamavenhibernate/HibernateUtil.java
+++ pos-java-maven-hibernate/src/main/java/posjavamavenhibernate/HibernateUtil.java
@@ -1,12 +1,10 @@
 package posjavamavenhibernate;
 
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class HibernateUtil {
-	
+public class HibernateUtil{
 	public static EntityManagerFactory factory = null;
 	
 	static {
@@ -14,19 +12,19 @@ public class HibernateUtil {
 	}
 	
 	private static void init() {
+	   
 		try {
-			
 			if(factory == null) {
 				factory = Persistence.createEntityManagerFactory("pos-java-maven-hibernate");
 			}
 			
-		}catch (Exception e) {
+		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
 		}
 	}
 	
-	public static EntityManager getEntityManager(){
-		return factory.createEntityManager(); //prove a persistencia
+	public static EntityManager getEntityManager() {
+		return factory.createEntityManager();//prove a parte de persistencia
 	}
 }
